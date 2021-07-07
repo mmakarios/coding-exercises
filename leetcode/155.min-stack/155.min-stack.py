ea6c13fb774641603333dd54e1b1d1dfb9280368
Michel <michel.makarios@gmail.com>
2021-07-07 00:01:00 -0300

    Add 155 min stack solution

---
+++ leetcode/155.min-stack/155.min-stack.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=155 lang=python3
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.minStack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
+    def getMin(self) -> int:
+        if self.stack:
+            return self.minStack[-1]
+        else:
+            return None
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
